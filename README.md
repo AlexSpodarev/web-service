c39fd9550ad90486fc5d14004b31db644173a6ce
Tommy Brecher <Tommy.Brecher@vonage.com>
2018-12-25 09:26:01 +0200

    Added requirments, changelog, readme and version files, optimized coded (see CHANGELOG.md)

---
+++ README.md
@@ -0,0 +1,25 @@
+This project is written in Python3
+
+Active Developers:
+- [@AlexSpodarev](https://github.com/AlexSpodarev)
+- [@tommybrecher](https://github.com/tommybrecher)
+
+If you wish to contribute, go ahead!
+
+
+### Getting started
+
+
+1) Create a virtualenv or use your local setup of Python3
+2) Install the requirments with pip (pip3 if you have both python2/3 installed) as following pip install -r requirments.txt
+3) Run the code: 
+   # Bash Style - Might need to chmod +x REST_API.py first
+   ./REST_API.py
+
+   # Python Style
+   python3 REST_API.py
+
+
+4) Acess the webserver on any of the IP addressess of the host, example: http://127.0.0.1:5000/api/<car_vendor>
+
+5) Enjoy :)
--- REST_API.py
+++ REST_API.py
@@ -6,85 +6,71 @@ from flask import abort
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def index():
-    return render_template('index.html', name='Alex')
-
-
-@app.route('/<param>', methods=['GET', 'POST'])
-def test(param):
-    if request.method == 'GET':
-        return f'You have requested {param}'
-
-    elif request.method == 'POST':
-        return f'You have posted {param}'
+def index(template='index.html', name='Alex'):
+    return render_template(template, name)
 
 
 @app.route('/admin')
 def reject():
-    abort(500)
+    reject_invalid_request(500)
 
 
 @app.route('/api/<data>', methods=['POST', 'GET'])
 def api(data):
-    # ! Code for POST method #########################################
+    """
+    This route is designed to handle api requests coming to the ur <server>/api/param
+    where param is a car Vendor, example: <server>/api/seat
+    """
     if request.method == 'POST':
+        """
+        When the request method is an HTTP/POST, the logic is to grab the data
+        Check if it's in the DB, if it's not, we append it to the DB
+        TODO: Duplicates/Case/Error handling should be added
+        """
         json = request.get_json()
-        exist_in_db_flag = 0
+
         for words in json:
-            entry = (data + "," + json[words] + "\n")
+            entry = f"{data},{json[words]}\n"
 
-        with open('database.csv', 'r') as f:
-            for line in f.readlines():
-                if line != '\n':
-                    # Checking if the received JSON content already exist in one of the rows of DB.(Flag)
-                    if line == entry:
-                        exist_in_db_flag = 1
-                        app.logger.info("The entry already exist, skipped")
-        # Now if the JSON is not in DB - we are writing/appending the row to the DB
-        if not exist_in_db_flag:
-            with open('database.csv', 'a') as f:
-                f.write(entry)
-                return f'<h1>Successfully saved to database: {entry}</h1>'
+        if content_in_db(entry):
+            return f'<h1>This Entry Already Exist</h1>'
+
         else:
-                return f'<h1>This entry already exist</h1>'
+            append_to_db(entry)
+            return f'<h1>Successfully saved {entry}</h1>'
 
-    # ! Code for GET method ##########################################
     elif request.method == 'GET':
-        with open('database.csv', 'r') as f:
-            result = []
-            for line in f.readlines():
-                if line != '\n':
-                    parsed = line.split(',')
-                    vendor = parsed[0].split()[0]
-                    model  = parsed[1].split()[0]
-                    if data == vendor:
-                        our_new_row_is = vendor + " " + model + '\n'
-                        result.append(our_new_row_is)
+        """
+        Logic for HTTP/GET
+        If the content exists in the DB, fetch it from the DB and return to user
+        """
+        if content_in_db(data):
+            reply = fetch_from_db(data)
+            return f'<h1>Found: {reply}</h1>'
 
-                        app.logger.debug("RESULT is:        %s" % result)
-                        app.logger.debug("Adding this line: %s" % our_new_row_is)
-                else:
-                    abort(404)
-            app.logger.debug("Almost final RESULT is: %s" % result)
-            result.insert(0, f'<h1>Found entries: {len(result)}</h1>')
-            app.logger.debug("       Final RESULT is: %s" % result)
-            app.logger.debug("   LENGTH of RESULT is: %s" % len(result))
+        else:
+            reject_invalid_request()
 
-            return """
-                    f'<h1>Found: {result}</h1>'
-                    
-                    """
+        app.logger.debug(f"[DEBUGGING]: Final RESULT is: {result}")
+        app.logger.debug(f"[DEBUGGING]: Le of RESULT is: {len(result)}")
+        result.insert(0, f'<h1>Found entries: {len(result)}</h1>')
 
-    # !  Code if not supported method #################################
     else:
-        return f'<h1>ERROR! Current request type isn\'t supported</h1>'
+        """
+        If method is not implemented reject with code 500 - (Internal Server Error)
+        """
+        reject_invalid_request(500)
 
 
-# ! Validation for INPUT messages like POST,PUT etc
 def content_validation(content):
-    if not request.json or not 'title' in request.json:
-        abort(400)
+    """
+    This function is used when validation of input is needed in 
+    HTTP methods which results in writing to us (server)
+    """
+    if not request.json or 'title' not in request.json:
+        reject_invalid_request(400)
     task = {
         'id': tasks[-1]['id'] + 1,
         'title': request.json['title'],
@@ -94,7 +80,7 @@ def content_validation(content):
     tasks.append(task)
     return jsonify({'task': task}), 201
 
-# @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+
 def update_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
@@ -108,10 +94,116 @@ def update_task(task_id):
     if 'done' in request.json and type(request.json['done']) is not bool:
         abort(400)
     task[0]['title'] = request.json.get('title', task[0]['title'])
-    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['description'] = request.json.get(
+        'description', task[0]['description'])
     task[0]['done'] = request.json.get('done', task[0]['done'])
     return jsonify({'task': task[0]})
 
 
+def append_to_db(content=None, db='database.csv', db_mode='a'):
+    """
+    The variable content starts as None which is the recommended way according to google code style.
+    if content is None, a log message is printed with the content and type(content)
+    The parameters db and db_mode are initialized to database.csv and a (append) respectivly,
+    this means this function is universable and the db/db_mode can be changed when calling the function
+    Example: append_to_db(content='some content: some value', db='new_db.csv', db_mode='w')
+    """
+    app.logger.info("Inside the function: append_to_db()")
+
+    if not content:
+        app.logger.info(f"Invalid content provided, content={content} type:{type(content)}")
+        reject_invalid_request(500)
+
+    else:
+        app.logger.info(f"Attempting to append content={content}, type:{type(content)} To: {db}")
+        with open(db, db_mode) as f:
+            f.write(content)
+            app.logger.info(f"Successfully appended content={content}, type:{type(content)} To: {db}")
+
+
+def content_in_db(content=None, db='database.csv', db_mode='r'):
+    app.logger.info("Inside the function: content_in_db()")
+    with open(db, db_mode) as f:
+        """
+        Opening the database.csv file and parsing line by line
+        if the line doesn't contain only a \n than it has data,
+        otherwise we realise that it is the end of the file and don't attempt
+        any processing on that line as that will result in an exception.
+        TODO: We will wan't to change this to an SQLite DB which is ran locally.
+        """
+        for line in f.readlines():
+            if line != '\n':
+                app.logger.info(f"Current Line: {line}")
+
+                if line.split(',')[0].split()[0] == content:
+                    """
+                    This chunk of code has the logic to check if the data recieved 
+                    In the HTTP/POST already exists in the DB.
+                    When the line already exists, exists_in_db is set to 1.
+                    which is used later on in this method to determine if further processing
+                    should be done (might want to use type:boolean)
+                    """
+                    app.logger.info("The entry already exist, skipped")
+                    return True
+
+            else:
+                app.logger.info(f"Couldn't find '{content}' in '{db}'")
+                return False
+
+
+def fetch_from_db(key=None, db='database.csv', db_mode='r'):
+    """
+    We pass in a key which starts out as None for error handling
+    the key is the Vendor of the car, we then open the DB
+    and check if the key matches the vendor column, if it does
+    we format the string and return it to the function which called
+    """
+    if key is not None:
+
+        with open(db, db_mode) as f:
+            for line in f.readlines():
+
+                if line != '\n':
+                    parsed = line.split(',')
+                    vendor = parsed[0].split()[0]
+                    model = parsed[1].split()[0]
+
+                    if vendor == key:
+                        """
+                        Those are strings so I can use .capitalize to make the first
+                        Letter an uppercase, ex: seat leon -> Seat Leon
+                        """
+                        return f'{vendor.capitalize()} {model.capitalize()}'
+
+                else:
+                    """
+                    If for some reason we reached the end of the file 
+                    and we can't find the key in the database we reject the request
+                    Code 501 - Internal Server Error
+                    """
+                    reject_invalid_request(501)
+
+    else:
+        """
+        If we didn't get a key reject the request with a 404 not found
+        """
+        reject_invalid_request()
+
+
+def reject_invalid_request(code=404):
+    """
+    This function is designed to reduce the lines of logic in the routes by 
+    Having a generic method to abort requests, when needed, just call reject_invalid_request
+    A log message will be printed and the request rejected with a 404 not found by default
+    Can be changed by calling the function with a different code example: reject_invalid_request(500)
+    """
+    app.logger.info(f"Invalid Request:{request} - Dropping with code: {code}")
+    abort(code)
+
+
 if __name__ == '__main__':
-    app.run(host='192.168.14.93', port="5000", debug=True)
+    """
+    If we hardcode the IP address of the host it will not run out of the box on new installations
+    Changing this to 0.0.0.0 to listen on all interfaces
+    """
+    app.run(host='0.0.0.0', port="5000", debug=True)
