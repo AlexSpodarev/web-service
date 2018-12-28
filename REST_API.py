e2cbfac07eb90595d8492803dc9e8179bf8e1d6d
AlexSpodarev <alex.spodarev@gmail.com>
2018-12-28 19:11:40 +0100

    Dear Tommy, i don't remember if this is the place where i should write my best regards for the look that you can throw on my code or.... or this is the place where i should write the changes. Anyways, please review my code :->

--- REST_API.py
+++ REST_API.py
@@ -1,109 +1,121 @@
 #!/usr/bin/env python
-from flask import Flask
-from flask import request
-from flask import render_template
-from flask import abort
+from flask import Flask, request, render_template, abort, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index(template='index.html', name='Alex'):
+def index(template='index.html', name='X'):
     return render_template(template, name)
 
 
-@app.route('/admin')
-def reject():
-    reject_invalid_request(500)
-
-
-@app.route('/api/<data>', methods=['POST', 'GET'])
-def api(data):
+@app.route('/api/<data>', methods=['POST'])
+def api_post(data):
     """
-    This route is designed to handle api requests coming to the url <server>/api/param
-    where param is a car Vendor, example: <server>/api/seat
+    When the request method is an HTTP/POST, the logic is to grab the data
+    Check if it's in the DB, if it's not, we append it to the DB
+    TODO: Duplicates/Case/Error handling should be added
     """
-    if request.method == 'POST':
-        """
-        When the request method is an HTTP/POST, the logic is to grab the data
-        Check if it's in the DB, if it's not, we append it to the DB
-        TODO: Duplicates/Case/Error handling should be added
-        """
-        json = request.get_json()
+    if data.split('/')[0].isalpha():
 
-        for words in json:
-            entry = f"{data},{json[words]}\n"
+        for words in request.get_json():
+            entry = f"{data},{words}\n"
+            app.logger.debug(entry)
 
         if content_in_db(entry):
-            return f'<h1>This Entry Already Exist</h1>'
+            return f'<h1>This entry already exist</h1>'
 
         else:
             append_to_db(entry)
             return f'<h1>Successfully saved {entry}</h1>'
+    else:
+        app.logger.info('Someone tried to POST non-alpha format')
+        reject_invalid_request(406)
 
-    elif request.method == 'GET':
-        """
-        Logic for HTTP/GET
-        If the content exist in the DB, fetch it from the DB and return to user
-        """
-        if content_in_db(data):
-            reply = fetch_from_db(data)
-            return f'<h1>Found: {reply}</h1>'
 
-        else:
-            reject_invalid_request()
-
-        app.logger.debug(f"[DEBUGGING]: Final RESULT is: {result}")
-        app.logger.debug(f"[DEBUGGING]: Le of RESULT is: {len(result)}")
-        result.insert(0, f'<h1>Found entries: {len(result)}</h1>')
+@app.route('/api/<data>', methods=['GET'])
+def api_get(data):
+    """
+    Logic for HTTP/GET
+    If the content exist in the DB, fetch it from the DB and return to user, else reject (404)
+    """
+    if is_in_db(data):
+        return fetch_from_db(data)
 
     else:
-        """
-        If method is not implemented reject with code 500 - (Internal Server Error)
-        """
-        reject_invalid_request(500)
+        reject_invalid_request()
 
 
-
-def update_task(task_id):
-    # At the moment the function is for beauty but it'll be impelemented in future versions
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    if not request.json:
-        abort(400)
-    if 'title' in request.json and type(request.json['title']) != unicode:
-        abort(400)
-    if 'description' in request.json and type(request.json['description']) is not unicode:
-        abort(400)
-    if 'done' in request.json and type(request.json['done']) is not bool:
-        abort(400)
-    task[0]['title'] = request.json.get('title', task[0]['title'])
-    task[0]['description'] = request.json.get(
-        'description', task[0]['description'])
-    task[0]['done'] = request.json.get('done', task[0]['done'])
-    return jsonify({'task': task[0]})
-
-
-def append_to_db(content=None, db='database.csv', db_mode='a'):
+def fetch_from_db(key=None, db='database.csv', db_mode='r'):
     """
-    The variable content starts as None which is the recommended way according to google code style.
-    if content is None, a log message is printed with the content and type(content)
-    The parameters db and db_mode are initialized to database.csv and a (append) respectivly,
-    this means this function is universable and the db/db_mode can be changed when calling the function
-    Example: append_to_db(content='some content: some value', db='new_db.csv', db_mode='w')
+    We pass in a key which starts out as None for error handling
+    the key is the Vendor of the car, we then open the DB
+    and check if the key matches the vendor column, if it does
+    we format the string and return it to the function which called
     """
-    app.logger.info("Inside the function: append_to_db()")
 
-    if not content:
-        app.logger.info(f"Invalid content provided, content={content} type:{type(content)}")
-        reject_invalid_request(500)
+    app.logger.info("Inside the function: fetch_from_db()")
+    Somelist = []
+    if key is not None:
 
+        with open(db, db_mode) as file:
+
+            for idx, line in enumerate(file.readlines()):
+                if idx == 0:
+                    header = line.strip().split(',')
+                    v1, v2, v3, v4, v5, v6 = header
+
+                elif line != '\n':
+                    parsed = line.split(',')
+                    vendor = parsed[0].split()[0]
+                    model = parsed[1].split()[0]
+                    year = parsed[2].split()[0]
+                    engine = parsed[3].split()[0]
+                    hp = parsed[4].split()[0]
+                    torque = parsed[5].split()[0]
+
+                    if vendor == key:
+                        Somelist.append(
+                            {
+                                v1: vendor,
+                                v2: model,
+                                v3: year,
+                                v4: engine,
+                                v5: hp,
+                                v6: torque
+                            }
+                        )
+
+                elif line == '\n':
+                    """
+                    If for some reason we reached the end of the file 
+                    and we can't find the key in the database we reject the request
+                    Code 501 - Internal Server Error
+                    """
+                    reject_invalid_request(501)
+
+            return jsonify(Somelist)
     else:
-        app.logger.info(f"Attempting to append content={content}, type:{type(content)} To: {db}")
-        with open(db, db_mode) as f:
-            f.write(content)
-            app.logger.info(f"Successfully appended content={content}, type:{type(content)} To: {db}")
+        """
+        If we didn't get a key, reject the request with a 404 not found
+        """
+        reject_invalid_request()
+
+
+def is_in_db(content=None, db='database.csv', db_mode='r'):
+    app.logger.info("Inside the function: is_in_db()")
+    result = []
+    with open(db, db_mode) as f:
+        for line in f.readlines():
+            if line != '\n':
+                if line.split(',')[0] == content:
+                    app.logger.info(f'Exist,appending:{line}')
+                    result.append(line)
+
+            else:
+                app.logger.info(f"Couldn't find '{content}' in '{db}'")
+                return result
+        return result
 
 
 def content_in_db(content=None, db='database.csv', db_mode='r'):
@@ -119,10 +131,11 @@ def content_in_db(content=None, db='database.csv', db_mode='r'):
         for line in f.readlines():
             if line != '\n':
                 app.logger.info(f"Current Line: {line}")
+                app.logger.info(f"Content     : {content}")
 
-                if line.split(',')[0].split()[0] == content:
+                if line == content:
                     """
-                    This chunk of code has the logic to check if the data recieved 
+                    This chunk of code has the logic to check if the data received 
                     In the HTTP/POST already exists in the DB.
                     When the line already exists, exists_in_db is set to 1.
                     which is used later on in this method to determine if further processing
@@ -136,43 +149,25 @@ def content_in_db(content=None, db='database.csv', db_mode='r'):
                 return False
 
 
-def fetch_from_db(key=None, db='database.csv', db_mode='r'):
+def append_to_db(content=None, db='database.csv', db_mode='a'):
     """
-    We pass in a key which starts out as None for error handling
-    the key is the Vendor of the car, we then open the DB
-    and check if the key matches the vendor column, if it does
-    we format the string and return it to the function which called
+    The variable content starts as None which is the recommended way according to google code style.
+    if content is None, a log message is printed with the content and type(content)
+    The parameters db and db_mode are initialized to database.csv and a (append) respectively,
+    this means this function is universal and the db/db_mode can be changed when calling the function
+    Example: append_to_db(content='some content: some value', db='new_db.csv', db_mode='w')
     """
-    if key is not None:
+    app.logger.info("Inside the function: append_to_db()")
 
-        with open(db, db_mode) as f:
-            for line in f.readlines():
-
-                if line != '\n':
-                    parsed = line.split(',')
-                    vendor = parsed[0].split()[0]
-                    model = parsed[1].split()[0]
-
-                    if vendor == key:
-                        """
-                        Those are strings so I can use .capitalize to make the first
-                        Letter an uppercase, ex: seat leon -> Seat Leon
-                        """
-                        return f'{vendor.capitalize()} {model.capitalize()}'
-
-                else:
-                    """
-                    If for some reason we reached the end of the file 
-                    and we can't find the key in the database we reject the request
-                    Code 501 - Internal Server Error
-                    """
-                    reject_invalid_request(501)
+    if not content:
+        app.logger.info(f"Invalid content provided, content={content} type:{type(content)}")
+        reject_invalid_request(500)
 
     else:
-        """
-        If we didn't get a key reject the request with a 404 not found
-        """
-        reject_invalid_request()
+        app.logger.info(f"Attempting to append content={content}, type:{type(content)} To: {db}")
+        with open(db, db_mode) as f:
+            f.write(content)
+            app.logger.info(f"Successfully appended content={content}, type:{type(content)} To: {db}")
 
 
 def reject_invalid_request(code=404):
@@ -189,6 +184,6 @@ def reject_invalid_request(code=404):
 if __name__ == '__main__':
     """
     If we hardcode the IP address of the host it will not run out of the box on new installations
-    Changing this to 0.0.0.0 to listen on all interfaces
+    Change this to 0.0.0.0 to listen on all interfaces
     """
     app.run(host='0.0.0.0', port="5000", debug=True)
